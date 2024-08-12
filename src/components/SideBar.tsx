56c68b110de9922c6901157c91411a8ccb79fccc
NEMYSESx <shakirraza4227@gmail.com>
2024-08-12 18:04:22 +0530

    commiting for deployment

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,12 +1,46 @@
 import { SideBarProps } from "../types/location";
 import DataCard from "./DataCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const SideBar = ({ myLocation, otherLocation }: SideBarProps) => {
   console.log(myLocation);
   console.log(otherLocation);
+
+  const [address, setAddress] = useState<string>("");
+
+  useEffect(() => {
+    setInterval(() => {
+      const fetchAddress = async () => {
+        if (myLocation?.latitude && myLocation?.longitude) {
+          const url = `https://geocode.maps.co/reverse?lat=${myLocation.latitude}&lon=${myLocation.longitude}&api_key=66b9135372750463292938hxo2c8caa`;
+
+          try {
+            const response = await axios.get(url);
+            if (response.data) {
+              setAddress(response.data.display_name);
+            }
+          } catch (error) {
+            console.error("Error fetching address:", error);
+          }
+        }
+      };
+      fetchAddress();
+    }, 10000);
+  }, [myLocation?.latitude, myLocation?.longitude]);
   return (
     <div className="fixed top-0 h-screen w-[355px] z-50 p-4 bg-gray-800 bg-opacity-75 text-white shadow-lg right-0">
-      <DataCard />
-      <DataCard />
+      <DataCard
+        isUser={true}
+        title="My Details"
+        address={address}
+        myLocation={myLocation}
+      />
+      <DataCard
+        isUser={false}
+        title="User Details"
+        otherLocation={otherLocation}
+      />
     </div>
   );
 };
