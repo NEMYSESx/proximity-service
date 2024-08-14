50e69afc3c75a5da34f42b3420f670c81d6a0dc3
NEMYSESx <shakirraza4227@gmail.com>
2024-08-15 03:13:51 +0530

    fixed bug

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -3,7 +3,7 @@ import { SignInButton } from "@clerk/clerk-react";
 const DashBoard = () => {
   return (
     <nav>
-      <SignInButton forceRedirectUrl="map/" />
+      <SignInButton forceRedirectUrl="map/12" />
     </nav>
   );
 };
--- src/pages/MapsPage.tsx
+++ src/pages/MapsPage.tsx
@@ -1,13 +1,13 @@
-import { SignedIn } from "@clerk/clerk-react";
+// import { SignedIn } from "@clerk/clerk-react";
 import Map from "../components/Map";
 
 const MapsPage = () => {
   return (
-    <SignedIn>
-      <div className="flex flex-col md:flex-row h-screen max-w-[1180px]">
-        <Map />;
-      </div>
-    </SignedIn>
+    // <SignedIn>
+    <div className="flex flex-col md:flex-row h-screen max-w-[1180px]">
+      <Map />;
+    </div>
+    // </SignedIn>
   );
 };
 
