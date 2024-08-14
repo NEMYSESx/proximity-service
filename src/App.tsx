901e3afb1e28fdc3926d51497ebae0129674c872
NEMYSESx <shakirraza4227@gmail.com>
2024-08-15 03:46:18 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import DashBoardPage from "./pages/DashBoardPage";
+// import DashBoardPage from "./pages/DashBoardPage";
 import MapsPage from "./pages/MapsPage";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <DashBoardPage />,
+      element: <MapsPage />,
     },
     {
       path: "/map",
-      element: <MapsPage />,
+      element: <></>,
     },
   ]);
   return (
