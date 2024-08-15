a38e6c4bd78cd39bf4af100cdc8064c901c91d7b
NEMYSESx <shakirraza4227@gmail.com>
2024-08-15 05:42:47 +0530

    testing

--- src/pages/MapsPage.tsx
+++ src/pages/MapsPage.tsx
@@ -1,16 +1,11 @@
-// import { SignedIn } from "@clerk/clerk-react";
 import Map from "../components/Map";
 
 const MapsPage = () => {
   return (
-    // <SignedIn>
     <div className="flex flex-col md:flex-row h-screen max-w-[1180px]">
       <Map />;
     </div>
   );
-  {
-    /* </SignedIn> */
-  }
 };
 
 export default MapsPage;
